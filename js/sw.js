f7568c01af47b93006b30852356cb4951920d961
Joaquin <joaquin.ariztia@smartsparrow.com>
2020-04-25 09:58:40 +1000

    Service worker and PWA details

---
+++ js/sw.js
@@ -0,0 +1,35 @@
+const staticCacheName = 'image-guess-v1';
+const assets = [
+  'index.html',
+  'js/index.js',
+  'css/index.css',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+];
+// install event
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log('caching shell assets');
+      cache.addAll(assets);
+    })
+  );
+});
+// activate event
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== staticCacheName)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+// fetch event
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request);
+    })
+  );
+});

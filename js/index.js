457ab333e2cc20a54a2e8bd2c94b8b43b6aa484c
Joaquin <joaquin.ariztia@smartsparrow.com>
2020-04-25 10:10:24 +1000

    Register service worker and HTML details for PWA

--- js/index.js
+++ js/index.js
@@ -78,6 +78,16 @@ document.addEventListener('DOMContentLoaded', function(event) {
 });
 
 ///////////////////////////////
+//  Register Service Worker  //
+///////////////////////////////
+
+if('serviceWorker' in navigator){
+  navigator.serviceWorker.register('/sw.js')
+    .then(reg => console.log('service worker registered'))
+    .catch(err => console.log('service worker not registered', err));
+}
+
+///////////////////////////////
 //  Prevent Backbutton Exit  //
 ///////////////////////////////
 
